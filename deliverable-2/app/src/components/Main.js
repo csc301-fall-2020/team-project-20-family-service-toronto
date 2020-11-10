be1a32dc95d9166d54e5af4518a2001cdf36ddeb
frank han <frank.han@mail.utoronto.ca>
2020-11-09 19:08:05 -0500

    Created a homepage template, added app logo images

--- deliverable-2/app/src/components/Main.js
+++ deliverable-2/app/src/components/Main.js
@@ -8,11 +8,31 @@ export default class Main extends Component {
   
 	render() {
     	return (
-    		<div className="App">
-		      <header className="App-header">
-		      	Hello
-      			<button onClick={() => this.route_Google()} type="submit"> Google </button>
+    		<div className="grid-container">
+		      <header>
+			  	<a href="/">Family Services Toronto Application</a>
 		      </header>
+			  <main>
+				<div className="content">
+					<button className="button" 
+						onClick={() => this.route_Google()} 
+						type="submit"> 
+						<img src={"images/google.png"} alt="google"></img> 
+					</button>
+					<button className="button"
+						type="submit"> 
+						<img src={"images/facebook.png"} alt="facebook"></img> 
+					</button>
+					<button className="button"
+						type="submit">
+						<img src={"images/youtube.png"} alt="youtube"></img> 
+					</button>
+					<button className="button"
+						type="submit">
+						<img src={"images/email.png"} alt="email"></img> 
+					</button>
+				</div>
+			  </main>
 		    </div>
     	);
   	}
