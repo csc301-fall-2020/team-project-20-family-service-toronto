26119b9fb76bf97c9004c07c964011d7e31b1b24
t.ferreria@live.ca <t.ferreira@live.ca>
2020-12-05 18:07:45 -0500

    Fixed front-end styling

--- deliverable-4/app/src/components/Main.js
+++ deliverable-4/app/src/components/Main.js
@@ -27,52 +27,66 @@ export default class Main extends Component {
 			win.focus();
 		}
 	}
+
+	get_curr_date = () => {
+	    var today = new Date();
+	    var hour = today.getHours();
+	    if (hour >= 0 && hour < 12) {
+	      return "Morning";
+	    } else if (hour >= 12 && hour < 18) {
+	      return "Afternoon";
+	    } else {
+	      return "Evening";
+	    }
+	  }
 	
 	render() {
+		this.phrase = this.get_curr_date();
+
 		return (
 			<div className="grid-container">
 				<header>
-					Family Services Toronto Application
+					Good {this.phrase}
 		      	</header>
 				<div className="container-fluid">
-					<div className="row">
+					<div className="row shadow-lg p-3 mb-5 bg-white rounded">
 						<div className="col-xs-12 col-md-6">
-							<button className="button"
+							<button className="button shadow p-3 mb-5 bg-white rounded"
 								onClick={() => this.route_Google()}
 								type="submit">
 								<img src={"images/google.png"} alt="google"></img>
 							</button>
 						</div>
 						<div className="col-xs-12 col-md-6">
-							<button className="button"
+							<button className="button shadow p-3 mb-5 bg-white rounded"
 								onClick={() => this.route_Facebook()}
 								type="submit">
 								<img src={"images/facebook.png"} alt="facebook"></img>
 							</button>
 						</div>
 						<div className="col-xs-12 col-md-6">
-							<button className="button"
+							<button className="button shadow p-3 mb-5 bg-white rounded"
 								onClick={this.route_YouTube}
 								type="submit">
 								<img src={"images/youtube.png"} alt="youtube"></img>
 							</button>
 						</div>
 						<div className="col-xs-12 col-md-6">
-							<button className="button"
+							<button className="button shadow p-3 mb-5 bg-white rounded"
 								onClick={() => this.route_Email()} 
 								type="submit">
 								<img src={"images/email.png"} alt="email"></img> 
 							</button>
 						</div>
 						<div className="col-xs-12 col-md-6">
-							<button className="button"
+							<button className="button shadow p-3 mb-5 bg-white rounded"
 								onClick={() => this.route_Zoom()}
 								type="submit">
 								<img src={"images/zoom.png"} alt="zoom"></img>
 							</button>
 						</div>
 						<div className="col-xs-12 col-md-6">
-							<button className="button"
+							<button className="button shadow p-3 mb-5 bg-white rounded"
 								onClick={this.route_WhatsApp}
 								type="submit">
 								<img src={"images/whatsapp.png"} alt="whatsapp"></img>
